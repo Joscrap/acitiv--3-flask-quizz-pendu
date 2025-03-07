0b0bc5a21ab962adf7a7374fad5cf40c83765a91
Joscrap <129535698+Joscrap@users.noreply.github.com>
2025-03-07 19:23:40 +0100

    début

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, session, redirect
+import os
+import random
+
+# Création de l'application
+app = Flask("Jeu du pandu")
+#création d"une clée secrete
+app.secret_key = os.urandom(24)
+
+# route de l'accueil
+@app.route('/')
+def index():
+    liste_de_mot = ["ordinateur", "valorant", "manger", "micro", "téléphone", "liste", "ordinateur", "classe", "papier", "arbre"]
+    mot_a_deviner = random.choice(liste_de_mot)
+    vies = 6
+
+    return redirect("/jeu")
+
+#affiche le jeu
+@app.route('/jeu')
+def jeu():
+    return render_template("index.html")
+
+# Exécution du code
+app.run(host='0.0.0.0', port=81)
